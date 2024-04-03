111cfc76d2d6f48656441b7b4bbd1c25147ec455
Hiroto Igarashi <hirotoigarashi@gmail.com>
2024-04-04 03:31:06 +0900

    update

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -6,7 +6,42 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  "AstroNvim/astrocommunity",
+  -- BAR/LINE PLUGINS
+  -- CODE RUNNER PLUGINS
+  -- COLOR PLUGINS
+  -- COLORSCHEMES
+  -- COMMENT PLUGINS
+  -- COMPLETION PLUGINS
+  -- DEBUGGIN PLUGINS
+  -- DIAGNOSTICS PLUGINS
+  -- EDITOR PLUGINS
+  -- FILE EXPLORE PLUGINS
+  -- GIT PLUGINS
+  -- INDENT PLUGINS
+  -- LSP PLUGINS
+  -- MARKDOWN/LATEX PLUGINS
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  -- MEDIA PLUGINS
+  -- MOTION PLUGINS
+  -- NOTE TAKING PLUGINS
+  -- PROGRAMMING LANGUAGE SUPPORT PLUGINS
+  -- PROJECT PLUGINS
+  -- REGISTER PLUGINS
+  -- REMOTE DEVELOPMENT PLUGINS
+  -- SCROLLING PLUGINS
+  -- SEARCH PLUGINS
+  -- SPLIT/WINDOWS PLUGINS
+  -- STARTUP PLUGINS
+  -- SYNTAX PLUGINS
+  -- TERMINAL INTEGRATION PLUGINS
+  -- TEST PLUGINS
+  -- UTILITY PLUGINS
+  -- WORKFLOW PLUGINS
+  -- PLUGIN PACKS
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
 }
