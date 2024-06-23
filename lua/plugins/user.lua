8ad3924a8b4fc534c155a3335406fb312589d717
Hiroto Igarashi <hirotoigarashi@gmail.com>
2024-06-24 03:15:34 +0900

    update user.lua

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
 
@@ -81,5 +79,10 @@ return {
         Rule("a", "a", "-vim")
       )
     end,
+  },
+  {
+    "williamboman/mason.nvim",
+    "mfussenegger/nvim-dap",
+    "jay-babu/mason-nvim-dap.nvim",
   },
 }
