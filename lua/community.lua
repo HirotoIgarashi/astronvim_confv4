6b1ca3e4e6b1a474ed1e95c8810c20e733d500c4
HirotoIgarashi <hirotoigarashi@gmail.com>
2024-09-01 23:08:11 +0900

    community.lua add lualine.nvim

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -9,6 +7,10 @@ return {
   -- import/override with your plugins folder
   "AstroNvim/astrocommunity",
   -- BAR/LINE PLUGINS
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
   -- CODE RUNNER PLUGINS
   -- COLOR PLUGINS
   -- COLORSCHEMES
