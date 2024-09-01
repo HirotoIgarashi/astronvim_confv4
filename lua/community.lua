ff6126111e239da6f3e4f5b1d3de49c81fde7666
HirotoIgarashi <hirotoigarashi@gmail.com>
2024-09-01 23:35:08 +0900

    community.lua add markmap.nvim

--- lua/community.lua
+++ lua/community.lua
@@ -6,10 +6,41 @@
 return {
   -- import/override with your plugins folder
   "AstroNvim/astrocommunity",
+
   -- BAR/LINE PLUGINS
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    options = { theme = "gruvbox" },
+    require("lualine").setup {
+      options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {},
+        always_divide_middle = true,
+        globalstatus = false,
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      extensions = {},
+    },
   },
   -- CODE RUNNER PLUGINS
   -- COLOR PLUGINS
@@ -26,6 +57,7 @@ return {
   -- LSP PLUGINS
   -- MARKDOWN/LATEX PLUGINS
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markmap-nvim" },
   -- MEDIA PLUGINS
   -- MOTION PLUGINS
   { import = "astrocommunity.motion.nvim-surround" },
